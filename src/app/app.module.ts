2fa8a8311da17b8a272c179ba0a93f6c76806f20
Joseph Lee <josephlee498@gmail.com>
2024-12-24 11:53:17 +0000

    change homepage component to movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomepageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, MoviesComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
