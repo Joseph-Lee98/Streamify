7fe480ce3dd18d3fe28e5af08269e52ef5988e19
Joseph Lee <josephlee498@gmail.com>
2024-12-24 14:18:55 +0000

    export isLoggedIn function to service, and generate authentication form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AuthenticationformComponent } from './authenticationform/authenticationform.component';
 
 @NgModule({
-  declarations: [AppComponent, MoviesComponent, NavbarComponent],
+  declarations: [AppComponent, MoviesComponent, NavbarComponent, AuthenticationformComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/authenticationform/authenticationform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authenticationform',
+  standalone: false,
+  
+  templateUrl: './authenticationform.component.html',
+  styleUrl: './authenticationform.component.css'
+})
+export class AuthenticationformComponent {
+
+}
