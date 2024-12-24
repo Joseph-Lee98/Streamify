97cf1c01acaff5faaf5c9014630be02b65c449f6
Joseph Lee <josephlee498@gmail.com>
2024-12-24 14:30:20 +0000

    routing for login and register pages set up, basic authform component set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
+import { AuthenticationformComponent } from './authenticationform/authenticationform.component';
 
 const routes: Routes = [
   {path:'', component:MoviesComponent},
+  {path:'login', component:AuthenticationformComponent},
+  {path:'register', component:AuthenticationformComponent},
 ];
 
 @NgModule({
